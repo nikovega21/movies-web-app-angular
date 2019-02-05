12d1cbde9e9cb4362563d99f8d6ab5152b8e8cb8
Manrique <manri.arf15@gmail.com>
2019-02-05 17:33:15 -0600

    module, for routes, movie details design

--- src/app/MaterialAngularModule.ts
+++ src/app/MaterialAngularModule.ts
@@ -1,9 +1,26 @@
 // tslint:disable-next-line:max-line-length
-import {MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatToolbarModule, MatFormFieldModule, MatRippleModule} from '@angular/material';
+import {MatIconModule, MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatToolbarModule, MatFormFieldModule, MatRippleModule} from '@angular/material';
+
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatToolbarModule, MatFormFieldModule, MatRippleModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatToolbarModule, MatFormFieldModule, MatRippleModule],
+  imports: [
+     MatIconModule, 
+     MatButtonModule,
+      MatCheckboxModule,
+       MatCardModule,
+       MatInputModule,
+        MatToolbarModule,
+        MatFormFieldModule,
+         MatRippleModule],
+  exports: [
+    MatIconModule,
+     MatButtonModule,
+     MatCheckboxModule,
+     MatCardModule,
+     MatInputModule,
+     MatToolbarModule,
+     MatFormFieldModule,
+     MatRippleModule],
 })
 export class MaterialModule { }
